eb7ade727f034d96fdc9042ad6f98c7b5cc256d9
Chris Olsen <chrisevjenolsen@gmail.com>
2022-09-27 16:29:21 -0400

    game logic added in playRound function java.js

--- java.js
+++ java.js
@@ -14,4 +14,53 @@ function getComputerChoice() {
     return choiceMove;
 }
 
+function playRound (playerSelection, computerSelection) {
+    
+    playerSelection = playerSelection.toLowerCase();
+    let roundEnd = "";
 
+    if (playerSelection == "rock") {
+        switch(computerSelection)
+        {
+            case "rock":
+                roundEnd = "It's a Draw!";
+                break;
+            case "paper":
+                roundEnd = "You Lose. Paper beats Rock!";
+                break;
+            case "scissor":
+                roundEnd = "You Win! Rock beats Scissors!";
+                break;
+        }
+    }
+    else if (playerSelection == "paper") {
+        switch(computerSelection)
+        {
+            case "rock":
+                roundEnd = "You Win! Paper beats Rock!";
+                break;
+            case "paper":
+                roundEnd = "It's a Draw!";
+                break;
+            case "scissor":
+                roundEnd = "You Lose. Scissors beats Paper";
+                break;
+        }
+    }
+    else if (playerSelection == "scissor") {
+        switch(computerSelection)
+        {
+            case "rock":
+                roundEnd = "You Lose. Rock beats Scissor!";
+                break;
+            case "paper":
+                roundEnd = "You Win! Scissor beats Paper!";
+                break;
+            case "scissor":
+                roundEnd = "It's a Draw!";
+                break;
+        }
+    }
+
+    console.log(roundEnd);
+}

61ecaca81f9f171accccfd25fcf67ff4050ed6ff
Chris Olsen <chrisevjenolsen@gmail.com>
2022-09-27 16:11:15 -0400

    function for computers randomly generated move added to java.js

--- java.js
+++ java.js
@@ -3,4 +3,15 @@
 - player inputs move (case insensitive) .toLowerCase()
 - console determines winner*/
 
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    let choiceMove = "";
+
+    if (choice == 0) { choiceMove = "rock";}
+    else if (choice == 1) {choiceMove = "paper";}
+    else if (choice == 2) {choiceMove = "scissor";}
+
+    return choiceMove;
+}
+
 
